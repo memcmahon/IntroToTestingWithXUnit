18de8ed1df7286cd032f721059f4bb6fd6336101
Megan McMahon <mcmahon.meganelizabeth@gmail.com>
2022-08-31 15:40:21 -0600

    Add project files.

---
+++ IntroToTesting/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToTesting
+{
+    public class User
+    {
+        public string Name { get; private set; }
+        public string Username { get; set; }
+        public List<String> Tweets { get; } = new List<String>();
+
+        public User(string name)
+        {
+            Name = name;
+        }
+
+        public void Tweet(string message)
+        {
+            Tweets.Add(message);
+        }
+
+        public string MostRecentTweet()
+        {
+            var lastIndex = Tweets.Count - 1;
+            return Tweets[lastIndex];
+        }
+    }
+}
